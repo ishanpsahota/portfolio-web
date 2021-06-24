ce6d722b00bb725f4dddf49f3a30d634ece786e7
Ishan Prasad <ishanprasad.sahota@gmail.com>
2021-06-25 02:26:19 +0530

    theme, logo, etc

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,36 +5,31 @@ import {
 import { useState  } from 'react'
 
 import classNames from "classnames";
+
+import logo from '../../assets/svgs/logo-transparent.svg'
   
-// function changeTheme() {    
-
-//     let theme = document.body.getAttribute('data-theme');
-
-//     if(theme === 'dark' ) {
-//         document.body.setAttribute('data-theme', 'light');
-//     }
-
-//     if(theme === 'light' ) {
-//         document.body.setAttribute('data-theme', 'dark');
-//     }
-// }
-
 function Navbar() {
 
     const [ theme, setTheme ] = useState(document.body.getAttribute('data-theme'));
 
     const iconClassLight = classNames({
-        'text-light': theme.match('light'),        
-        'text-dark': theme.match('dark'),
+        'text-dark': theme.match('light'),        
         'bi-sun-fill': theme.match('light'),
         'bi-sun': theme.match('dark')
     })
 
-    const iconClassDark = classNames({
-        'text-dark': theme.match('light'),        
-        'text-light': theme.match('dark'),
+    const iconClassDark = classNames({        
+        'text-dark': theme.match('light'),
         'bi-moon-fill': theme.match('dark'),
         'bi-moon': theme.match('light')
+    })
+
+    const themeSwitchClass = classNames({
+        'form-check': true,
+        'form-switch': true,
+        'icon-stack-wrapper': true,
+        'bg-dark': theme.match('dark'),
+        'bg-light': theme.match('light')
     })
 
     var changeTheme = (e) => {        
@@ -48,30 +43,34 @@ function Navbar() {
             document.body.setAttribute('data-theme', 'dark');
             setTheme('dark')
         }
-    }
-
-    // var checkTheme = (e) => {
-    //     if(document.body.getAttribute('data-theme') === 'dark') return true
-        
-    //     return false
-    // }
+    }    
 
     return (
         <Router>
-            <nav className="nav-wrapper">
-                <ul className="nav-list">
-                    <li className="nav-link">
-                        <Link to="/" className="navbar-brand"> Ishan Prasad </Link>
-                    </li>
-                    <li className="nav-link">
-                        <div className="form-check form-switch icon-stack-wrapper">
-                            <label className="form-check-label" > <i className={iconClassLight}  ></i> </label>
-                            <input className="form-check-input" onChange={changeTheme} checked={theme === 'dark' ? true : false} type="checkbox" />
-                            <label className="form-check-label" > <i className={iconClassDark}  ></i>  </label>
-                        </div>
-                    </li>
-                </ul>
-            </nav>        
+            <nav className="navbar navbar-expand-lg navbar-dark nav-wrapper border-theme">
+                <div className="container-fluid">
+                    <Link to="/" className="navbar-brand text-special">
+                        <img src={logo} alt="Ishan Prasad" />
+                    </Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse justify-content-end navbar-collapse" id="navbar">
+                        <ul className="navbar-nav  mb-2 p-3 p-lg-0 mb-lg-0">
+                            <li className="nav-item mx-auto">
+                                <div className={themeSwitchClass}>
+                                    <label className="form-check-label" > <i className={iconClassLight}  ></i> </label>
+                                    <input className="form-check-input" onChange={changeTheme} checked={theme === 'dark' ? true : false} type="checkbox" />
+                                    <label className="form-check-label" > <i className={iconClassDark}  ></i>  </label>
+                                </div>
+                            </li>
+                            {/* <li className="nav-item">
+                                
+                            </li> */}                                            
+                        </ul>                
+                    </div>
+                </div>
+            </nav>
         </Router>        
     )
 }
--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -2,7 +2,7 @@
 function Home() {
     return (
         <div className="home-wrapper">
-            <div className="row full-view gradient">
+            <div className="row full-view gradient hero-polygon-wrapper">
                 
             </div>
         </div>
